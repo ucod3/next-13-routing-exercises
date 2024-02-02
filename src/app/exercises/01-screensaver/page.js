d124eb75dc8794d575ab6982b01c6ecfa0f96ca9
Usman Butt <usman.butt@outlook.com>
2024-02-02 08:59:08 -0500

    Refactor screensaver exercise page to include color options

--- src/app/exercises/01-screensaver/page.js
+++ src/app/exercises/01-screensaver/page.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import Link from 'next/link';
 
-import ScreenSaver from '../../../components/ScreenSaver';
-
-function ScreenSaverExercise() {
+function ScreenSaverMainPage() {
   return (
-    <main className="screen-saver-wrapper">
-      <ScreenSaver color="red" />
+    <main className='screen-saver-wrapper'>
+      <h1>Choose your color:</h1>
+      <ul>
+        <li>
+          <Link href='/exercises/01-screensaver/red'>Red</Link>
+        </li>
+        <li>
+          <Link href='/exercises/01-screensaver/blue'>Blue</Link>
+        </li>
+        <li>
+          <Link href='/exercises/01-screensaver/green'>Green</Link>
+        </li>
+      </ul>
     </main>
   );
 }
 
-export default ScreenSaverExercise;
+export default ScreenSaverMainPage;
