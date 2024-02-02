540258ef451f9213bd4f2dbd74b62aae1aa84972
Usman Butt <usman.butt@outlook.com>
2024-02-02 14:22:32 -0500

    Add toast message and redirect on form submission

--- src/app/exercises/02-flash-messages/contact/page.js
+++ src/app/exercises/02-flash-messages/contact/page.js
@@ -1,15 +1,34 @@
 'use client';
 import React from 'react';
+import { useRouter } from 'next/navigation';
+import Toast from '../../../../components/Toast/';
+import { ToastContext } from '../../../../components/ToastProvider';
 
 function ContactPage() {
+  const context = React.useContext(ToastContext);
+  if (!context) {
+    throw new Error('Toast must be rendered within a ToastProvider component');
+  }
+  const { createToast } = context;
+  const router = useRouter();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    router.push('/exercises/02-flash-messages/');
+    createToast(
+      `Your message was received. We'll get back to you shortly!`,
+      'success',
+    );
+  };
+
   return (
     <main>
-      <form>
-        <label htmlFor="name">Name:</label>
-        <input id="name" required={true} />
+      <form onSubmit={handleSubmit}>
+        <label htmlFor='name'>Name:</label>
+        <input id='name' required={true} />
 
-        <label htmlFor="message">Message:</label>
-        <textarea id="message" />
+        <label htmlFor='message'>Message:</label>
+        <textarea id='message' />
 
         <button>Submit</button>
       </form>
